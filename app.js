097c46891fdce1ad1c982530294f0c4b7d21d732
Chiqo Rifky Saputra <chiqors@outlook.com>
2023-02-07 10:06:41 +0700

    updated with improved less resource and time consumption

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let arr = [];
-
 rl.question('Nama? ', (nama) => {
     rl.question('Email? ', (email) => {
         rl.question('NoTelp? ', (notelp) => {
             console.log(`Terimakasih ${nama} sudah memasukkan data!`);
             const data = fs.readFileSync('data/contacts.json', 'utf-8');
-            if(data.length > 0) {
-                arr = JSON.parse(data);
-            }
+            const arr = JSON.parse(data);
             arr.push({ nama, email, notelp });
             fs.writeFileSync('data/contacts.json', JSON.stringify(arr));
             rl.close();
